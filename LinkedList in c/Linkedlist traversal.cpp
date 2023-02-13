0f2bcc05382aeb4ab98d74a452a80068c0856eef
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-02-13 15:14:48 +0530

    Create Linkedlist traversal.cpp

---
+++ LinkedList in c/Linkedlist traversal.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<stdlib.h>
+	struct node{
+int data;
+struct node*next;
+};
+void linkedlistTraversal(struct node*ptr){
+while(ptr!=NULL){
+printf("Element:%d\n",ptr->data);
+ptr=ptr->next;
+}
+}
+int main(){
+struct node*head;
+struct node*second;
+struct node*third;
+struct node*four;
+//allocate memory for nodes in the linked list in head
+head=(struct node*)malloc(sizeof(struct node));
+second=(struct node*)malloc(sizeof(struct node));
+third=(struct node*)malloc(sizeof(struct node));
+four=(struct node*)malloc(sizeof(struct node));
+//link first nd second nodes
+head->data=7;
+head->next=second;
+//link second and third nodes
+second->data=11;
+second->next=third;
+//terminate list at third
+third->data=66;
+third->next=four;
+//link third to fourth
+four->data=81;
+four->next=NULL;
+linkedlistTraversal(head);
+return 0;
+}
