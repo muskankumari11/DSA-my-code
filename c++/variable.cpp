dde3fdb0d1519ac67feb480e3b3c1830a99923e7
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-04-11 07:48:09 +0530

    Create variable.cpp

---
+++ c++/variable.cpp
@@ -0,0 +1,12 @@
+//how variable is made
+#include <iostream>
+using namespace std;
+int main() {
+int a=4,b=5;
+float pi=3.14;
+char c='d';
+cout<<"\nthe value of b is"<<b;
+cout<<"\nvalue of pi is "<<pi;
+cout<<"\nthe value of c is:"<<c;
+		return 0;
+}
