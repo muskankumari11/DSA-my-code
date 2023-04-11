69dbaad3011e470613991a1074bf0ade7a48ea3b
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-04-11 09:15:12 +0530

    Create variable1.cpp

---
+++ c++/variable1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a=2,b=4;
+char ch='b';
+bool is_true=true;
+cout<<"the bool value is:"<<false;
+	return 0;
+}
+output- the bool value is:0
